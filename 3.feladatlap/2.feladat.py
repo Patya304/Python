a0f4f01cd8a0439d94531723a45c47ed317b7ae1
md_patrik <xd2.0.hun@gmail.com>
2023-03-26 16:25:05 +0200

    Delete 3.feladatlap directory

--- 3.feladatlap/2.feladat.py
+++
@@ -1,7 +0,0 @@
-"""
-2. feladat: Írj programot, mely egy beolvasott számra kiírja
-a) annak kétszeresét
-a szám 10-zel nagyobb értékét
-"""
-num = int(input("Kérek egy számot: "))
-print(f"A szám kétszerese: {num*2} \nA számtól tizzel nagyobb szám: {num+10}")
--- 3.feladatlap/5.feladat.py
+++
@@ -1,7 +0,0 @@
-"""
-5. feladat: Írj programot, mely miután megkérdezi, hogy hány éves vagy, annyiszor kiírja (új soronként), hogy: LEGYEN SZÉP NAPOD!
-"""
-kor = int(input("Hány éves vagy: "))
-
-for i in range(1, kor+1):
-    print("Legyen szép napod!")
