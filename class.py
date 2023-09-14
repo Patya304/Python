8339dc0141a5777ae1bd7781b270e0efdaf00b4c
md_patrik <xd2.0.hun@gmail.com>
2023-09-14 14:59:52 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,1 +1,11 @@
+class Eu:
+  def __init__(self, sor):
+    
+    
 
+
+    open("szopok.txt", encoding = "latin2")
+    lista = []
+    for s in jozf:
+      rekord = Eu(s)
+      lista.append(rekord)
