35c0cf334c1e1e08ec518f1cc32efee2f98b0136
md_patrik <xd2.0.hun@gmail.com>
2023-11-23 14:28:09 +0100

    Update test.py

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -1,27 +1,4 @@
 import streamlit as st
 
-bekeres1 = st.number_input(label="Adj meg egy számot")
- 
-bekeres2 = st.number_input(label="Adj meg egy másik számot")
-
-
-menu = st.radio("Menu",
-                ("Összeadas", "Kivonas", "Szorzas", "Osztas"))
-
-ertek = 0
-
-def calculate():
-    if menu == "Összeadás":
-        ertek = bekeres1 + bekeres2
-    elif menu == "Kivonás":
-        ertek = bekeres1 - bekeres2
-    elif menu == "Szorzás":
-        ertek = bekeres1 * bekeres2
-    elif menu == "Osztás" and bekeres2!=0:
-        ertek = bekeres1 / bekeres2
-    else:
-        st.warning("0-val nem osztunk")
-        ertek = "?????"
-     st.success(f"Valasz = {ertek}")
-if st.button("Eredmény kiszámítása"):
-    calculate()
+adat1 = st.number_input("Kérek egy számot")
+adat2 = st.number_input("Kérek még egy számot")
