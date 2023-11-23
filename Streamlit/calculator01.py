4ca24c02881a49da612596689989e8c2eb7877e6
md_patrik <xd2.0.hun@gmail.com>
2023-11-23 09:08:34 +0100

    Update calculator01.py

--- Streamlit/calculator01.py
+++ Streamlit/calculator01.py
@@ -10,3 +10,26 @@ num1 = st.number_input(label="Adj megy egy számot")
  
 # adat2
 num2 = st.number_input(label="Adj meg még egy számot")
+
+
+st.write("Operátor")
+ 
+operation = st.radio("Menü:",
+                    ("Összeadás", "Kivonás", "Szorzás", "Osztás")
+
+ans = 0
+ 
+def calculate():
+    if operation == "Összeadás":
+        ans = num1 + num2
+    elif operation == "Kivonás":
+        ans = num1 - num2
+    elif operation == "Szorzás":
+        ans = num1 * num2
+    elif operation=="Osztás" and num2!=0:
+        ans = num1 / num2
+    else:
+        st.warning("0-val való osztás hibája. Adjon meg egy nem nulla számot.")
+        ans = "Nem definiálható "
+ 
+    st.success(f"Megoldás = {ans}")
