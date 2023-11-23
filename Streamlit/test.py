351b51efbb0dffb5e441ac8031052fc1942c06be
md_patrik <xd2.0.hun@gmail.com>
2023-11-23 14:31:18 +0100

    Update test.py

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 adat1 = st.number_input("Kérek egy számot")
 adat2 = st.number_input("Kérek még egy számot")
+
+muveletek = st.radio("Menu",
+                     ("+","-","*", ":"))
