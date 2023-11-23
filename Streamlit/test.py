844f29dde5159949977c7e91207dbcff2f597abe
md_patrik <xd2.0.hun@gmail.com>
2023-11-23 14:05:39 +0100

    Update test.py

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -6,3 +6,23 @@ bekeres2 = st.numoer_input(label="szam2")
 
 menu = st.radio("Menu",
                 ("Összeadas", "Kivonas", "Szorzas", "Osztas"))
+
+ertek = 0
+
+def calculate():
+    if menu == "Összeadás":
+        ertek = bekeres1 + bekeres2
+    elif menu == "Kivonás":
+        ertek = bekeres1 - bekeres2
+    elif menu == "Szorzás":
+        ertek = bekeres1 * bekeres2
+    elif menu == "Osztás" and bekeres2!=0:
+        ertek = bekeres1 / bekeres2
+    else:
+        st.warning("0-val nem osztunk")
+        ertek = "?????"
+
+      st.success(f"Valasz = {ertek}")
+ 
+if st.button("Eredmény kiszámítása"):
+    calculate()
