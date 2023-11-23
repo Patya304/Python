70bb6a85e3473130459cb2372534059c3d74f58d
md_patrik <xd2.0.hun@gmail.com>
2023-11-23 09:05:31 +0100

    Update calculator01.py

--- Streamlit/calculator01.py
+++ Streamlit/calculator01.py
@@ -1,6 +1,12 @@
 import streamlit as st
  
-st.title("Calculator App using Streamlit")
+st.title("Calculator")
  
 # creates a horizontal line
 st.write("---")
+
+# adat1
+num1 = st.number_input(label="Adj megy egy számot")
+ 
+# adat2
+num2 = st.number_input(label="Adj meg még egy számot")
