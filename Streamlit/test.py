021c0f3f74d54881140e300602911df556131387
md_patrik <xd2.0.hun@gmail.com>
2023-11-23 14:03:33 +0100

    Update test.py

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 bekeres1 = st.number_input(label="Szam1")
 bekeres2 = st.numoer_input(label="szam2")
+
+
+menu = st.radio("Menu",
+                ("Összeadas", "Kivonas", "Szorzas", "Osztas"))
