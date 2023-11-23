3b26ad9db26d9bb59b73c043b425df6e5fc7595a
md_patrik <xd2.0.hun@gmail.com>
2023-11-23 14:36:12 +0100

    Update test.py

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
+ertek = 0
 adat1 = st.number_input("Kérek egy számot")
 adat2 = st.number_input("Kérek még egy számot")
 
 muveletek = st.radio("Menu",
                      ("+","-","*", ":"))
+
+def szamologep():
+  if muveletek == "+":
+    ertek = adat1 + adat2
+  elif muveletek == "-":
+    ertek = adat1 - adat2
+  elif muveletek == "*":
+    ertek = adat1 * adat2
+  elif 
